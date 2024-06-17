a9928e62196f8adc1224c83bbaedf2b8e69d1316
Tiago Benaias Pompeo <tiapom@gmail.com>
2024-06-17 14:39:20 +0100

    Init Commit

---
+++ TopMoviesMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TopMoviesMaui;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ TopMoviesMaui/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TopMoviesMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
